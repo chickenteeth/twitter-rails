6736c80fec335afd237c879b25f14337798cb3fa
Tristan Stutzman <tristan.stutzman@gmail.com>
2020-07-05 23:53:11 -0700

    add users name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name, :email, :password
 end
